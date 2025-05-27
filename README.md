83dd87974961812e21c778f0fb377bda00592d99
tyql688 <yury.zero.21@gmail.com>
2025-05-27 17:00:58 +0800

    Initial

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# helloworld
+# astrbot_plugin_wwuid_reply_enhance
 
-AstrBot 插件模板
+基于astrbot的wwuid的回复增强。
 
-A template plugin for AstrBot plugin feature
+## 主要功能
 
-# 支持
+1. 基于astrbot的`引用回复`上传角色面板图
+2. 目前仅支持图片引用。
 
-[帮助文档](https://astrbot.app)
+
+## 感谢
+
+- 本项目仅供学习使用，请勿用于商业用途
+- [WutheringWavesUID](https://github.com/tyql688/WutheringWavesUID)
--- main.py
+++ main.py
@@ -1,24 +1,71 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
+import re
+from typing import List
+
 from astrbot.api import logger
+from astrbot.api.event import AstrMessageEvent, filter
+from astrbot.api.star import Context, Star, register
+from astrbot.core.config.astrbot_config import AstrBotConfig
+from astrbot.core.message.components import Image, Plain, Reply
+from astrbot.core.star.filter.event_message_type import EventMessageType
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+
+@register(
+    "wwuid_reply_enhance",
+    "tyql688",
+    "基于astrbot的wwuid的回复增强。",
+    "1.0.0",
+    "https://github.com/tyql688/astrbot_plugin_wwuid_reply_enhance",
+)
+class WwuidReplyEnhance(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.prefix: List[str] = config.PREFIX  # type: ignore
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.event_message_type(EventMessageType.ALL, priority=114514)
+    async def on_all_message(self, event: AstrMessageEvent):
+        if not any(event.message_str.startswith(prefix) for prefix in self.prefix):
+            logger.debug(
+                f"wwuid_reply_enhance: {event.message_str} not start with {self.prefix}"
+            )
+            return
 
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        pattern = r"上传.*?面板图$"
+        match = re.search(pattern, event.message_str)
+        if not match:
+            logger.debug(
+                msg=f"wwuid_reply_enhance: {event.message_str} not match {pattern}"
+            )
+            return
+
+        # 解析引用内容
+        imgs = []
+        for _message in event.get_messages():
+            if isinstance(_message, Reply) and _message.chain:
+                for comp in _message.chain:
+                    if isinstance(comp, Image):
+                        event.message_obj.message.append(comp)
+                        imgs.append(comp)
+
+        if not imgs:
+            return
+
+        # 开始伪造消息
+        message_obj = event.message_obj
+        message_obj.message = imgs
+        message_obj.message.append(Plain(text=event.message_str))
+
+        event = AstrMessageEvent(
+            message_str=event.message_str,
+            message_obj=message_obj,
+            platform_meta=event.platform_meta,
+            session_id=event.session_id,
+        )
+
+        logger.debug(
+            f"wwuid_reply_enhance fake: message_str:{event.message_str}, message_obj:{event.message_obj}"
+        )
+
+        self.context.get_platform(event.get_platform_name()).commit_event(event)
+
+        event.stop_event()
+        return
